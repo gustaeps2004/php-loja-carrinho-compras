d574d8fcb01dc33987b60ecfc48cd9a332b29708
gustaeps2004 <gustaeps@gmail.com>
2025-09-17 20:21:55 -0300

    adicionando redirecionamento após compra

--- assets/functions/finalizarPagamentoCarrinho.php
+++ assets/functions/finalizarPagamentoCarrinho.php
@@ -4,12 +4,11 @@ use APP\Controllers\PedidosController;
 
 $pedidosController = $container->get(PedidosController::class);
 
-$usuarioID = 
-
 $pedidosController->finalizar(
   (int)$_GET["usuarioID"],
   (int)$_GET["metodoPagamento"]
 );
 
-
+header("Location: ../../html/carrinho/carrinho.php?usuarioID=".$_GET["usuarioID"])."&compraFinalizada=true";
+exit;
 ?>
